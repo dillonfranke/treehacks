73339e4585c153947556e13d4ff1c14ed8725cf4
dillonfranke <dfranke@wafbla.org>
2017-02-18 11:38:49 -0800

    Update file_upload.php

--- file_upload.php
+++ file_upload.php
@@ -4,12 +4,16 @@
    		$file = $_FILES['file'];
    		print_r('file');
 
-   		//File Properties
+   		/*//File Properties
    		$file_name = $file['name'];
    		$file_tmp = $file['tmp_name'];
    		$file_size = $file['size'];
 
    		//Work out the file extension
    		$file_ext = explode('.', $file_name);
+   		$file_ext = strtolower(end($file_ext));
+
+   		$file_name_new = uniqid('', true) . '.' , $file_ext;*/
    }
+
 ?>
