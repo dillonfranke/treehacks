3f9ff60f481baac7e066e26506f9c2346f9b7569
dillonfranke <dfranke@wafbla.org>
2017-02-18 13:08:00 -0800

    Update file_upload.php

--- file_upload.php
+++ file_upload.php
@@ -7,7 +7,9 @@
    	$file_size = $file['size'];
    	$file_error = $file['error'];
     
-    $file_destination = 'uploads/' . $file_name;
+    $file_name_new = uniqid('', true) . '.' . $file_ext;
+    
+    $file_destination = 'uploads/' . $file_name_new;
 
     if (move_uploaded_file($file_tmp, $file_destination)) {
       echo $file_destination;
