e293c065003f529256428be6c079d64c32498af9
dillonfranke <dfranke@wafbla.org>
2017-02-18 12:43:40 -0800

    Update

--- file_upload.php
+++ file_upload.php
@@ -2,8 +2,10 @@
 
   if (isset($_FILES['file'])) {
     $file = $_FILES['file'];
-    print_r($file);
+    $file_name = $file['name'];
+   	$file_tmp = $file['tmp_name'];
+   	$file_size = $file['size'];
+   	$file_error = $file['error'];
   }
-  echo "test";
 
 ?>
