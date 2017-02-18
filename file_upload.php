e9768a4e20585405d2f0253dfa5752b6be8c6cc7
dillonfranke <dfranke@wafbla.org>
2017-02-18 12:24:16 -0800

    Update file_upload.php

--- file_upload.php
+++ file_upload.php
@@ -2,7 +2,6 @@
 
    if (isset($_FILES['file'])) {
    		$file = $_FILES['file'];
-   		print_r('file');
 
    		//File Properties
    		$file_name = $file['name'];
@@ -18,21 +17,17 @@
 
    		$allowed = array('mp4', 'mov', 'wmv');
 
-   		if (in_array($file_ext, $allowed)) {
-   			if ($file_error === 0) {
+   		//if (in_array($file_ext, $allowed)) {
+   			//if ($file_error === 0) {
 
-   				$file_name_new = uniqid('', true) . '.' . $file_ext;
+   				
    				$file_destination = 'uploads/' . $file_name_new;
 
    				if (move_uploaded_file($file_tmp, $file_destination)) {
    					echo $file_destination;
    				}
 
-   			} else {
-   				echo "Error";
    			}
-   		} else {
-   			echo "Error";
    		}
    }
 
